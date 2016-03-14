8e01f471a29cd4db158d2ad14ad5429b5385f9b9
Trevor McGrath <trevormcgrath88@gmail.com>
2016-03-14 15:38:51 -0700

    Added a grunt task for my JS, now minifies it and reloads the page on a save.

--- js/LandingPageScript.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-
-	console.log("jQuery v1.12.0 loaded");
-
-	
-});
---
+++ library/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+	console.log("jQuery v1.12.0 loaded");
+	var one = "hello"; //var 1
+	var two = "world"; // var 2
+	var three = "begin"; // var 3
+	var five = "coding"; // var 4
+
+	//alert(one);
+
+
+
+});
