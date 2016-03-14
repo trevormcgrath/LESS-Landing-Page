3bcadb53e62e4b079fc23774ee601f28816b2e99
Trevor McGrath <trevormcgrath88@gmail.com>
2016-03-14 10:30:18 -0700

    Playing around and learning with LESS

--- css/gruntfile.js
+++ css/gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
           optimization: 2
         },
         files: {
-          "LandingPageStyles.css": "styles.less" // destination file and source file
+          "LandingPageStyles.css": "main.less" // destination file and source file
         }
       }
     },
